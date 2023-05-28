1892b983947dd90f90c1eefa7c4ab1747c5b57fd
Summer Chen <mp4bj6ruby@gmail.com>
2023-05-29 00:39:48 +0800

    login update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,11 +2,11 @@ import LoginInput from '@/components/LoginInput'
 import React from 'react'
 function LoginPage() {
   return (
-    <main className="flex items-center justify-center bg-[url('/img/bg_login.jpg')] bg-center bg-cover h-full">
-      <div className="">
+    <div className="flex items-center justify-center bg-[url('/img/bg_login.jpg')] bg-center bg-cover bg-fixed overflow-y-auto">
+      <div className="py-10">
         <div className="icon logo_login mx-auto mb-8" />
         <div className="icon img_joinus mx-auto mb-6"></div>
-        <div className="icon tag_login_base pt-[3px] mb-10">
+        <div className="icon tag_login_base pt-[3px] mb-8">
           <div className="flex items-center">
             <div className="btn-base w-[135px] flex justify-center items-center">
               <div className="btn-text">登入</div>
@@ -21,7 +21,7 @@ function LoginPage() {
           icon="ic_login_id"
           placeholder="請輸入會員帳號"
           autocomplete="new-password"
-          className="mb-5"
+          className="mb-4"
         />
         <LoginInput
           icon="ic_login_pw"
@@ -35,14 +35,16 @@ function LoginPage() {
               type="checkbox"
               className="border border-white text-yellow-500 bg-transparent"
             />
-            <span className="ml-2 text-white tracking-wider">記住帳密</span>
+            <span className="ml-2 text-white tracking-wider text-sm">
+              記住帳密
+            </span>
           </label>
 
-          <a href="#" className="link text-white">
+          <a href="#" className="link text-white text-sm">
             忘記密碼
           </a>
         </div>
-        <div className="icon btn_login_base mt-6 flex items-center justify-center">
+        <div className="icon btn_login_base mt-5 flex items-center justify-center">
           <div className="btn-text">登入</div>
         </div>
 
@@ -50,7 +52,7 @@ function LoginPage() {
           <a href="#" className="text-white link">
             先去逛逛
           </a>
-          <span className="border-r border-white h-12"></span>
+          <span className="border-r border-white h-10"></span>
           <a href="#" className="text-white link">
             在線客服
           </a>
@@ -84,7 +86,7 @@ function LoginPage() {
           </div>
         </div>
       </div>
-    </main>
+    </div>
   )
 }
 
