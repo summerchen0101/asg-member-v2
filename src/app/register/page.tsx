3eb5a79dc836e8162c1233124bfa24171c4c0127
Summer Chen <mp4bj6ruby@gmail.com>
2023-05-29 01:14:06 +0800

    register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -8,10 +8,10 @@ function LoginPage() {
         <div className="icon img_joinus mx-auto mb-6"></div>
         <div className="icon tag_login_base pt-[3px] mb-8 px-[2px]">
           <div className="flex items-center">
-            <div className="btn-base h-9 w-[140px] flex justify-center items-center">
+            <div className="flex-1 justify-center items-center">
               <div className="btn-text">登入</div>
             </div>
-            <div className="flex-1 flex justify-center items-center">
+            <div className="btn-base h-9 w-[140px] flex justify-center items-center">
               <div className="btn-text">註冊</div>
             </div>
           </div>
@@ -29,63 +29,50 @@ function LoginPage() {
             password
             autocomplete="new-password"
           />
+          <LoginInput
+            icon="ic_login_cf"
+            placeholder="請確認密碼"
+            password
+            autocomplete="new-password"
+          />
+          <LoginInput
+            icon="ic_login_phone"
+            placeholder="請輸入手機號碼"
+            addonAfter={
+              <div className="bg-yellow-500 rounded-full h-7 px-3 py-1 text-white text-sm drop-shadow-lg">
+                獲取驗證碼
+              </div>
+            }
+          />
+          <LoginInput icon="ic_login_vf" placeholder="請輸入手機驗證碼" />
+          <LoginInput
+            icon="ic_login_agno"
+            placeholder="請輸入經銷商號碼(選填)"
+          />
         </div>
-
-        <div className="mt-5 flex justify-between w-[280px] px-3">
+        <div className="mt-5">
           <label>
             <input
               type="checkbox"
               className="border border-white text-yellow-500 bg-transparent"
             />
             <span className="ml-2 text-white tracking-wider text-sm">
-              記住帳密
+              我已年滿18歲並同意相關規範及開戶協議
             </span>
           </label>
-
-          <a href="#" className="link text-white text-sm">
-            忘記密碼
-          </a>
         </div>
         <div className="icon btn_login_base mt-5 flex items-center justify-center">
-          <div className="btn-text">登入</div>
+          <div className="btn-text">註冊</div>
         </div>
 
         <div className="space-x-6 text-center my-8 -ml-4">
           <a href="#" className="text-white link">
-            先去逛逛
+            已有帳號
           </a>
           <span className="border-r border-white h-10"></span>
           <a href="#" className="text-white link">
             在線客服
           </a>
-        </div>
-        <div className="bg-[rgba(255,255,255,0.2)] rounded-full w-[280px] h-12 flex items-center px-6">
-          <div className="icon logo_appdnld mr-2"></div>
-          <div className="text-white text-sm">
-            <div className="">
-              <span className="">下載FA8 APP</span>
-              <span className="link float-right">立即下載</span>
-            </div>
-            <div className="">百萬玩家信賴 隨時隨地暢玩</div>
-          </div>
-        </div>
-        <div className="flex gap-x-6 items-center mt-6 px-2">
-          <div className="flex flex-col items-center">
-            <div className="icon ic_info1 mb-2"></div>
-            <div className="text-gray-200 text-xs">加密安全</div>
-          </div>
-          <div className="flex flex-col items-center">
-            <div className="icon ic_info2 mb-2"></div>
-            <div className="text-gray-200 text-xs">海量賽事</div>
-          </div>
-          <div className="flex flex-col items-center">
-            <div className="icon ic_info3 mb-2"></div>
-            <div className="text-gray-200 text-xs">極速存取</div>
-          </div>
-          <div className="flex flex-col items-center">
-            <div className="icon ic_info4 mb-2"></div>
-            <div className="text-gray-200 text-xs">裝置兼容</div>
-          </div>
         </div>
       </div>
     </div>
--- src/components/LoginInput.tsx
+++ src/components/LoginInput.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import React, { ReactNode, useState } from 'react'
 import cs from 'classnames'
 
 type Props = {
@@ -10,6 +10,7 @@ type Props = {
   autocomplete?: string
   icon?: string
   className?: string
+  addonAfter?: ReactNode
 }
 function LoginInput({
   value,
@@ -19,19 +20,20 @@ function LoginInput({
   autocomplete,
   icon,
   className,
+  addonAfter,
 }: Props) {
   const [type, setType] = useState<'text' | 'password'>('password')
   return (
     <div
       className={cs(
-        'bg-[rgba(255,255,255,0.4)] w-[280px] h-12 rounded-full px-3 flex items-center btn',
+        'bg-[rgba(255,255,255,0.4)] w-[280px] h-12 rounded-full px-3 flex items-center btn focus:[box-shadow:none] relative',
         className,
       )}
     >
       {icon ? <div className={cs('icon ic_login_id mr-3', icon)}></div> : null}
 
       <input
-        className="placeholder:text-[rgba(255,255,255,0.8)] bg-transparent flex-1 outline-none text-white border-none focus:outline-none"
+        className="placeholder:text-[rgba(255,255,255,0.8)] bg-transparent flex-1 outline-none text-white border-none focus:outline-none px-0"
         placeholder={placeholder}
         value={value}
         onChange={onChange}
@@ -51,6 +53,7 @@ function LoginInput({
           ></div>
         )
       ) : null}
+      <div className="absolute right-0 mr-2">{addonAfter}</div>
     </div>
   )
 }
