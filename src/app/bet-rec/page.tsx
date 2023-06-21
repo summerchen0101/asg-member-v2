407af492f1a8dccc51de4e68731aea3263b83e7b
Summer Chen <mp4bj6ruby@gmail.com>
2023-06-21 17:07:31 +0800

    bet_rec search

--- src/app/bet-rec/page.tsx
+++ src/app/bet-rec/page.tsx
@@ -1,6 +1,7 @@
 import AmountSelector from '@/components/AmountSelector'
 import AppLayout from '@/components/AppLayout'
 import BankCard from '@/components/BankCard'
+import CalenderInput from '@/components/CalenderInput'
 import CsvSelector from '@/components/CsvSelector'
 import DateRangeSelector from '@/components/DateRangeSelector'
 import SelectInput from '@/components/SelectInput'
@@ -18,14 +19,43 @@ function BetRecPage() {
         value="record"
       />
       <div className="p-2">
-        <div className="white-box h-48">
+        <div className="white-box pb-6">
           <WhiteBoxTitle>查詢</WhiteBoxTitle>
           <div className="p-6 space-y-4">
             <SelectInput
               label="遊戲平台"
               options={mapToOpts({ all: '全部', zg: 'ZG', gr: '好路' })}
             />
+            <CalenderInput label="開始日期" />
+            <CalenderInput label="結束日期" />
           </div>
+          {/* <DateRangeSelector /> */}
+          <div className="bg-gray-100 rounded mx-4 pt-1 pb-2 px-3">
+            <div className="text-gray-600 mb-2">快速查詢</div>
+            <div className="grid grid-cols-3 gap-1 mx-auto w-max">
+              <div className="icon btn_yellow6 text-white text-shadow text-center italic pt-1 scale-95">
+                今天
+              </div>
+              <div className="icon btn_white6 text-gray-500 italic text-center pt-1  scale-95">
+                昨天
+              </div>
+              <div className="icon btn_white6 text-gray-500 italic text-center pt-1  scale-95">
+                本週
+              </div>
+              <div className="icon btn_white6 text-gray-500 italic text-center pt-1  scale-95">
+                上週
+              </div>
+              <div className="icon btn_white6 text-gray-500 italic text-center pt-1  scale-95">
+                本月
+              </div>
+              <div className="icon btn_white6 text-gray-500 italic text-center pt-1  scale-95">
+                自選
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="icon btn_a2_base text-primary-600 font-medium tracking-widest text-center mx-auto mt-4 pt-2">
+          查詢
         </div>
       </div>
     </AppLayout>
---
+++ src/components/CalenderInput.tsx
@@ -0,0 +1,18 @@
+import { OptionType } from '@/lib/types'
+import React from 'react'
+
+type Props = {
+  label?: string
+}
+function CalenderInput({ label }: Props) {
+  return (
+    <div className="border-b border-slate-300 flex px-2 pb-1">
+      <div className="text-slate-500 font-medium">{label}:</div>
+      <div className="flex flex-1 items-center justify-end">
+        <input type="date" className="border-0 m-0 py-0 text-slate-400" />
+      </div>
+    </div>
+  )
+}
+
+export default CalenderInput
