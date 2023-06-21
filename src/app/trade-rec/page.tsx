310ec49d57bc40ff41fb09d6ce827a3ea3675528
Summer Chen <mp4bj6ruby@gmail.com>
2023-06-21 16:01:21 +0800

    empty rec

--- src/app/trade-rec/page.tsx
+++ src/app/trade-rec/page.tsx
@@ -20,6 +20,16 @@ function TradeRecPage() {
         <DateRangeSelector />
       </div>
 
+      {/* <div className="flex flex-col items-center justify-center h-[calc(100%-150px)]">
+        <div className="icon ic_nodata"></div>
+        <div className="text-slate-400 text-base mt-1 tracking-wider">
+          暫無紀錄
+        </div>
+        <div className="mt-4 icon btn_yellow5 pt-2 text-center text-white text-shadow italic">
+          立即存款
+        </div>
+      </div> */}
+
       <div className="p-2 space-y-2.5">
         <TradeRecord />
         <TradeRecord expend />
