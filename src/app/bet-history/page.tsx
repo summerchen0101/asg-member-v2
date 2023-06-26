50eb510155d418d7eaa33d4554105792b113ca14
Summer Chen <mp4bj6ruby@gmail.com>
2023-06-26 11:06:19 +0800

    bet-history

---
+++ src/app/bet-history/page.tsx
@@ -0,0 +1,88 @@
+import AppLayout from '@/components/AppLayout'
+import TabGroup from '@/components/TabGroup'
+import { getWeekDates, mapToOpts } from '@/utils'
+import { startOfWeek } from 'date-fns'
+
+function BetRecPage() {
+  const thisWeekDays = getWeekDates()
+  const lastWeekDays = getWeekDates(1)
+  return (
+    <AppLayout title="投注紀錄">
+      <TabGroup
+        options={mapToOpts({ record: '投注紀錄', history: '歷史帳務' })}
+        value="history"
+      />
+      <div className="p-3">
+        <table className="custom-table">
+          <thead>
+            <tr>
+              <th>日期</th>
+              <th>投注金額</th>
+              <th>有效投注</th>
+              <th>輸贏</th>
+            </tr>
+          </thead>
+          <tbody>
+            {thisWeekDays.map((t) => (
+              <tr key={t}>
+                <td>{t}</td>
+                <td>0.00</td>
+                <td>0.00</td>
+                <td>
+                  <div className="flex items-center justify-center">
+                    <div className="">0.00</div>
+                    <div className="icon ic_arrow ml-2"></div>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+          <thead>
+            <tr>
+              <th>本週統計</th>
+              <th>0.00</th>
+              <th>0.00</th>
+              <th className="pr-4">0.00</th>
+            </tr>
+          </thead>
+        </table>
+
+        <table className="custom-table mt-3">
+          <thead>
+            <tr>
+              <th>日期</th>
+              <th>投注金額</th>
+              <th>有效投注</th>
+              <th>輸贏</th>
+            </tr>
+          </thead>
+          <tbody>
+            {lastWeekDays.map((t) => (
+              <tr key={t}>
+                <td>{t}</td>
+                <td>0.00</td>
+                <td>0.00</td>
+                <td>
+                  <div className="flex items-center justify-center">
+                    <div className="">0.00</div>
+                    <div className="icon ic_arrow ml-2"></div>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+          <thead>
+            <tr>
+              <th>本週統計</th>
+              <th>0.00</th>
+              <th>0.00</th>
+              <th className="pr-4">0.00</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </AppLayout>
+  )
+}
+
+export default BetRecPage
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,11 @@
 import { OptionType, StringMap } from '@/lib/types'
+import {
+  eachDayOfInterval,
+  endOfWeek,
+  format,
+  startOfWeek,
+  sub,
+} from 'date-fns'
 import numeral from 'numeral'
 
 export const toCurrency = (num = 0, decimal = 0) =>
@@ -16,4 +23,21 @@ export const mapToOpts = function <T>(
       value: (isNaN(+value) ? value : +value) as unknown as T,
     }),
   )
+}
+
+export function getWeekDates(offsetWeeks = 0) {
+  let now = new Date()
+
+  let startOfThisWeek = startOfWeek(now, { weekStartsOn: 1 }) // 1 for Monday
+  let endOfThisWeek = endOfWeek(now, { weekStartsOn: 1 })
+
+  let startOfTargetWeek = sub(startOfThisWeek, { weeks: offsetWeeks })
+  let endOfTargetWeek = sub(endOfThisWeek, { weeks: offsetWeeks })
+
+  let weekDates = eachDayOfInterval({
+    start: startOfTargetWeek,
+    end: endOfTargetWeek,
+  })
+
+  return weekDates.map((date) => format(date, 'yyyy-MM-dd'))
 }
